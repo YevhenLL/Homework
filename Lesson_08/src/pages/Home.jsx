5d22ac0acd79c4bae7408757e6d48067c4be33cc
YevhenLL <yevhenlash@gmail.com>
2025-02-26 20:45:14 +0100

    Update project folders

--- Lesson_08/src/pages/Home.jsx
+++ Lesson_08/src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import CountryForm from '../components/CountryForm/CountryForm';
+import { useSelector } from 'react-redux';
 
 export default function Home() {
+  const { countries } = useSelector(state => state.countries);
+  
   return (
     <div className="home-page">
       <div className="grey-background">
         <h3>Home Component 🏡</h3>
       </div>
-      <CountryForm />
+      {countries.length > 0 && <CountryForm />}
     </div>
   );
 }
